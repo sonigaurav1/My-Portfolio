9d8c5812ad27863edb16acaca3387f9c50a8a1c8
Gaurav Soni <gauravsoni7763@gmail.com>
2024-03-19 20:57:47 +0545

    menu bar issue

--- src/components/header/page.js
+++ src/components/header/page.js
@@ -83,16 +83,16 @@ export default function NavigationBar() {
         </span>
         <ul>
           <li>
-            <a href="#home">Home</a>
+            <a onClick={() => handleMenuClose()} href="#home">Home</a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a onClick={() => handleMenuClose()} href="#about">About</a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a onClick={() => handleMenuClose()} href="#projects">Projects</a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a onClick={() => handleMenuClose()} href="#contact">Contact</a>
           </li>
         </ul>
       </div>
